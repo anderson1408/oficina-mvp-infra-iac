59bbd3994f6a2081dd3129adad006813a5c48b83
mduques <mduques@gmail.com>
2026-07-20 21:16:19 -0300

    style: aplica terraform fmt no eks.tf e ajusta definicao do cluster

--- eks.tf
+++ eks.tf
@@ -10,8 +10,8 @@ data "aws_iam_role" "lab_role" {
 # 2. DEFINIÇÃO DO CLUSTER EKS
 # ==============================================================================
 resource "aws_eks_cluster" "main" {
-  name     = "${var.project_name}-cluster"
-  
+  name = "${var.project_name}-cluster"
+
   # Usa a role nativa do Lab em vez de tentar criar uma nova
   role_arn = data.aws_iam_role.lab_role.arn
 
@@ -27,18 +27,18 @@ resource "aws_eks_cluster" "main" {
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.project_name}-node-group"
-  
+
   # Os nós (máquinas) também usam a mesma LabRole existente
-  node_role_arn   = data.aws_iam_role.lab_role.arn
-  
+  node_role_arn = data.aws_iam_role.lab_role.arn
+
   # Aloca as máquinas nas subnets da VPC Default
-  subnet_ids      = data.aws_subnets.default.ids
+  subnet_ids = data.aws_subnets.default.ids
 
   # Configuração de Auto-Scaling básica para Laboratório
   scaling_config {
-    desired_size = 2  # Começa com 2 máquinas ativas
-    max_size     = 3  # Permite escalar até 3 se houver sobrecarga
-    min_size     = 1  # Mantém no mínimo 1 máquina viva
+    desired_size = 2 # Começa com 2 máquinas ativas
+    max_size     = 3 # Permite escalar até 3 se houver sobrecarga
+    min_size     = 1 # Mantém no mínimo 1 máquina viva
   }
 
   # Instância custo-benefício para Lab
